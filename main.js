0108b3a62e0769db2211b95923810728340075ae
C05_Basem <basemhmoud735@gmail.com>
2021-05-12 23:28:39 +0300

    Create a middleware function logMethod

--- main.js
+++ main.js
@@ -5,20 +5,24 @@ const port = 3000;
 app.use(express.json());
 const users = ["John", "Mark"];
 //pulse check q1
-const logUsers =(req, res, next)=>{
-    console.log(users);
-    next()
-}
-app.use(logUsers)
+const logUsers = (req, res, next) => {
+  console.log(users);
+  next();
+};
+//pulse check q2
+//application-level middleware
+app.use(logUsers);
 
+//pulse check q3
+const logMethod = (req, res, next) => {
+  console.log(req.method);
+  next();
+};
+app.use(logMethod);
 app.get("/users", (req, res, next) => {
   res.json(users);
 });
 
-
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
